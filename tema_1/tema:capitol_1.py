8ff66d286ff8c441ec17eacb5c4a9b11233a4aae
krishicban <reyod2003@gmail.com>
2025-08-07 20:23:50 +0300

    homework1

---
+++ tema_1/tema:capitol_1.py
@@ -0,0 +1,14 @@
+#kinds of variables
+#using input
+city = str(input("\nWhere is your current City? "))
+print ("I'm currently living in ",city)
+print (type(city))
+year_birth = int(input("\nWhat year you are born? "))
+print ("I was born on the year " ,year_birth)
+print (type(year_birth))
+height = float(input("\nHow tall are you in meters? "))
+print ("I'm ",height,"meters tall")
+print (type(height))
+civil_status = bool (input("\nAre you married? "))
+print (civil_status)
+print (type(civil_status))
