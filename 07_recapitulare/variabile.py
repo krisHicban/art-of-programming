2a5b7678596c263edabd677f6b5452a1bf8a9e2c
lao.water <baciu.raul555@gmail.com>
2025-10-09 06:29:29 +0300

    preparing some recap

---
+++ 07_recapitulare/variabile.py
@@ -0,0 +1,7 @@
+# No type declarations required - Python infers types dynamically
+name = "Python"  # string
+age = 30         # integer
+price = 19.99    # float
+is_valid = True  # boolean (note the capitalization)
+
+x, y, z = 1, 2, 3
