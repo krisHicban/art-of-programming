d6d00874393c8916972dbd221997a4a4fe78f4ed
RaresKeY <158580472+RaresKeY@users.noreply.github.com>
2025-10-18 10:48:24 +0300

    refactor: remove api key c

--- Students Tasks/CosminP/DjBlueResearch.py
+++ Students Tasks/CosminP/DjBlueResearch.py
@@ -92,7 +92,7 @@ WHISPER_LANGUAGE = None
 
 # ============ Tokens ============
 HF_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
-AAI_API_KEY = os.environ.get("ASSEMBLYAI_API_KEY", "7336f1ceb4374c008f7ffa1b225edb83")
+AAI_API_KEY = os.environ.get("ASSEMBLYAI_API_KEY`")
 
 
 # =====================================================================
