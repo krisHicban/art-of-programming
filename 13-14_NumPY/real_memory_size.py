d6f4e784ab7837680d67c8a5ccf41eb6d2a3b067
RaresKeY <158580472+RaresKeY@users.noreply.github.com>
2025-10-31 14:40:48 +0200

    chore: underscore number clarity

--- 13-14_NumPY/real_memory_size.py
+++ 13-14_NumPY/real_memory_size.py
@@ -3,12 +3,12 @@ import numpy as np
 
 
 stars_python = []
-for i in range(100000):
+for i in range(100_000):
     distance = (i**2 + (i*2)**2)**0.5
     stars_python.append(distance)
 
 
-positions = np.arange(100000, dtype=np.float64)
+positions = np.arange(100_000, dtype=np.float64)
 distances = np.sqrt(positions**2 + (positions*2)**2)
 
 python_memory_real = asizeof.asizeof(stars_python)
